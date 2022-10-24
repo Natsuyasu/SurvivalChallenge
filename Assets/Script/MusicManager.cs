117137164f9074f20e8c3f85a807a8849ba4d3f8
Natsuyasu <2542135846@qq.com>
2022-10-25 02:21:44 +0800

    add3

---
+++ Assets/Script/MusicManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    [SerializeField] AudioClip musicOnStart;
+
+    AudioSource audioSource;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void Start()
+    {
+        Play(musicOnStart, true);
+    }
+
+    AudioClip swichTo;
+
+    public void Play(AudioClip music, bool interrupt = false)
+    {
+        if(interrupt == true)
+        {
+            volume = 1f;
+            audioSource.volume = volume;
+            audioSource.clip = music;
+            audioSource.Play();
+        }
+        else
+        {
+            swichTo = music;
+            StartCoroutine(SmoothSwichMusic());
+        }
+        
+        
+    }
+
+    float volume;
+    [SerializeField] float timeToSwich;
+
+    IEnumerator SmoothSwichMusic()
+    {
+        volume = 1f;
+
+        while(volume > 0f)
+        {
+            volume -= Time.deltaTime / timeToSwich;
+            if (volume < 0f) { volume = 0f; }
+            audioSource.volume = volume;
+            yield return new WaitForEndOfFrame();
+        }
+
+        Play(swichTo, true);
+    }
+
+
+}
