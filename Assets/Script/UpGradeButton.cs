4af7b37ea2a9d6a60399ecad26d5bf9bcf51a405
Natsuyasu <2542135846@qq.com>
2023-04-09 01:11:29 +0800

    almost finish

--- Assets/Script/UpGradeButton.cs
+++ Assets/Script/UpGradeButton.cs
@@ -2,19 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class UpGradeButton : MonoBehaviour
+public class UpGradeButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     [SerializeField] Image icon;
+    [SerializeField] Text text;
+    private string UpgradeName;
 
     public void Set(UpGradeData upGradeData)
     {
         icon.sprite = upGradeData.icon;
+        UpgradeName = upGradeData.Name;
     }
 
     internal void Clean()
     {
         icon.sprite = null;
     }
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        // 鼠标悬停在按钮上时，在文本框中显示对象名称
+        text.text = UpgradeName;
+    }
 
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        // 鼠标移出按钮时，清空文本框
+        text.text = "";
+    }
 }
